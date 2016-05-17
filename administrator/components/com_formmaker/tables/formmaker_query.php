a80626aa7b5f3c5f5ec301d5a800235526c1fc88
N6REJ <troy@hallhome.us>
2016-05-16 22:29:31 -0500

    removed formmaker

--- administrator/components/com_formmaker/tables/formmaker_query.php
+++
@@ -1,4 +0,0 @@
-<?php  /** * @package Form Maker * @author Web-Dorado * @copyright (C) 2011 Web-Dorado. All rights reserved. * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html **/defined('_JEXEC') or die('Restricted access');
-class Tableformmaker_query extends JTable{	var $id = null;	var $form_id = null;	var $query = null;	var $details = null;		function __construct(&$db)	{		parent::__construct('#__formmaker_query','id',$db);	}}
-
-?>
--- administrator/components/com_formmaker/uninstall.mysql.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE `#__formmaker`;
-DROP TABLE `#__formmaker_submits`;
-DROP TABLE `#__formmaker_views`;
-DROP TABLE `#__formmaker_themes`;
-DROP TABLE `#__formmaker_sessions`;
-DROP TABLE `#__formmaker_blocked`;
-DROP TABLE `#__formmaker_query`;
